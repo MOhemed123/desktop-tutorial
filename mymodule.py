5e601ab6aebc7e12715c8217615e225cfd2878ab
MOhemed123 <87959435+MOhemed123@users.noreply.github.com>
2023-01-20 00:56:04 +0200

    Add files via upload

---
+++ mymodule.py
@@ -0,0 +1,16 @@
+def add(x,y):
+    print (f'sum =  {x+y}') 
+
+def sub(x,y):
+    print(x-y)
+
+def mult(x,y):
+    print(f'sub = {x*y}')
+
+def divd(x,y):
+    if y==0:
+        print(" no  divide by zero ")
+    else:
+        print(f'divid =  {x/y}')
+
+
